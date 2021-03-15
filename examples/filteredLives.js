674770322b5b8f59e6b9487cc94a1586a701a87d
Matt <mnaglak@umich.edu>
2021-03-15 09:52:22 -0400

    links to database page added

--- examples/filteredLives.js
+++ examples/filteredLives.js
@@ -71,6 +71,7 @@ function timestamp(str) {
 	var personProvinceStamp = null; //entranceDate data for individaul search for
 	var searchActive = false; //boolean for if a search is currently taking place
 	var personStringYear= '';
+	var url = 'https://jesuit-lives.lontracanadensis.net/catalog/'
 //create popup boxes for polylines as well as the timestamps for each moment of importance; timestamps are needed for current date filter system
 //runs only when search is performed
 	function forEachFeature(f, layer) {
@@ -78,6 +79,7 @@ function timestamp(str) {
 		f.properties.deathStamp = timestamp(f.properties.Death_Date);
 		f.properties.provinceStamp = timestamp(f.properties.Entrance_Date_1);
 		personStringYear = f.properties.yearOfEntry.toString();
+
 
 		var out = [];
 		if (f.properties) {
@@ -91,6 +93,7 @@ function timestamp(str) {
 			out.push('Date of Death: ' + f.properties.Death_Date);
 			out.push('Place of Death: ' + f.properties.Place_of_Death);
 			out.push('Entry Year: ' + personStringYear);
+			out.push('<a href="'+ url + f.properties.Id + '" target="_blank">Database Entry</a>');  //allows for link to external URL via attribute in .geoJson table
 		layer.bindPopup(out.join("<br />"));
 		}
 
